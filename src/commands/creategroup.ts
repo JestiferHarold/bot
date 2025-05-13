54e7e9aee6204047472e5ee22026fd50de1b8b59
JestiferHarold <Sanjay.Pandian@tutamail.com>
2025-05-14 01:53:08 +0530

    create Group Chat, testing needed

---
+++ src/commands/creategroup.ts
@@ -0,0 +1,59 @@
+import { Chat, Client, Contact, ContactId, CreateGroupResult, GroupParticipant, Message } from "whatsapp-web.js";
+
+async function createGroupChat(wwclient : Client, message : Message) : Promise<Message | void>{
+    const participants : Array<Contact> | Array<GroupParticipant> | Array<ContactId> | Array<string> = await message.getMentions()
+    const chat : Chat = await message.getChat()
+
+    if (!chat.isGroup) {
+        return
+    }
+
+    let leaveChat : boolean = false
+    let name : string;
+
+    if (participants.length != 0 && /\d/.test(message.body[(message.body.indexOf("@") + 1)])) {
+
+        name = message.body.slice(2, message.body.indexOf("@"))
+
+    } else if (message.body.slice(message.body.length - 10, message.body.length).toLowerCase() != "--everyone") {
+
+        leaveChat = true
+        name = message.body.slice(2, message.body.length - 10)
+
+        //@ts-ignore
+        participants = chat.participants
+
+        for (let num = 0; num < participants.length; num ++) {
+            //@ts-ignore
+            participants[num] = participants[num].id._serialized //not sure what string array I need to pass either serialized or just the number
+        }
+
+    } else {
+        return
+    }
+
+    //@ts-ignore
+    const group : CreateGroupResult | string = await wwclient.createGroup(
+        name, 
+        //@ts-expect-error
+        participants,
+        {
+            autoSendInviteV4 : true,
+            comment : `Join ${name}`
+        }
+    )
+
+    if (leaveChat) {
+        //@ts-ignore
+        await wwclient.getChatById(group.gid).leave()
+    }
+
+    //This message should be sent as a personal message to the user who used this command not in the group chat
+
+    return await wwclient.sendMessage(message.from, "Group Created")
+    
+    //,cb <name of the new group> ( --everyone || @userMentions )
+    //one rule the name of the group should not have "@" 
+    //example := ,cg cse 114 asd as asd das @mentions  || ,cg cse 115 asdf asdefds --everyon
+
+}
